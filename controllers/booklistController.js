cd923aaf330ad537b3805221744290a180c93c80
Pauline McGinnis <73722071+pmcginnisIT@users.noreply.github.com>
2021-02-18 22:05:57 -0500

    Update booklistController.js

--- controllers/booklistController.js
+++ controllers/booklistController.js
@@ -51,7 +51,7 @@ router.put('/update/:entryId', validateSession, function (req, res) {
 router.get('/bookList', validateSession, (req, res) => {
     
     Books.findAll({
-        where: {bookList: true, owner: req.user.id}
+        where: {}
     })
     .then(book => res.status(200).json(book))
     .catch(err => res.status(500).json({error: err})) 
